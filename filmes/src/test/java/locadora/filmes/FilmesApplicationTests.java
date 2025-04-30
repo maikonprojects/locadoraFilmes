90036e66baf55d2b8f3ba5fa436c1a0c0c5a00eb
maikonprojects <162837965+maikonprojects@users.noreply.github.com>
2025-04-30 11:35:36 -0300

    Subindo estrutura do projeto

---
+++ filmes/src/test/java/locadora/filmes/FilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package locadora.filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
